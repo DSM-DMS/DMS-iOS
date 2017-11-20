a92fa3171f4d235e4465cd77bf7584728bc1d2c2
half-but <qudcks9191@naver.com>
2017-11-20 23:58:35 +0900

    add func get data, bind

--- DSMapp/ViewController/Apply/ApplyStudyVC.swift
+++ DSMapp/ViewController/Apply/ApplyStudyVC.swift
@@ -8,8 +8,14 @@ import UIKit
 
 class ApplyStudyVC: UIViewController  {
 
-    @IBOutlet weak var contentView: BackViewShape!
+    @IBOutlet weak var backScrollView: UIScrollView!
     @IBOutlet weak var changeRoomButton: UIButton!
+    
+    var selectedTime = 11
+    var selectedClass = 1
+    var selectedSeat = 0
+    
+    var beforeButton: UIButton? = nil
     
     @IBAction func pressBack(_ sender: Any) {
         back()
@@ -22,13 +28,18 @@ class ApplyStudyVC: UIViewController  {
     }
     
     override func viewDidLoad() {
-        contentView.backgroundColor = Color.CO6.getColor()
+        backScrollView.backgroundColor = Color.CO6.getColor().withAlphaComponent(0.5)
+        backScrollView.layer.cornerRadius = 8
         changeRoomButton.addTarget(self, action:
             #selector(changeRoom(_:)), for: .touchUpInside)
     }
     
     @IBAction func timeChange(_ sender: UISegmentedControl) {
-        
+        if sender.selectedSegmentIndex == 0{
+            selectedTime = 11
+        }else{
+            selectedTime = 12
+        }
     }
     
 }
@@ -49,8 +60,73 @@ extension ApplyStudyVC{
         for i in 0..<roomNameArr.count{
             if action.title == roomNameArr[i]{
                 changeRoomButton.setTitle(action.title, for: .normal)
+                selectedClass = i+1
                 return
             }
         }
     }
+    
+    func getMap(){
+        connector(add: "/extension/\(selectedTime)", method: "GET", params: ["class" : "\(selectedClass)"], fun: {
+            data, code in
+            switch code{
+            case 200:
+                self.showToast(msg: "로드 성공")
+                self.bindData(try! JSONSerialization.jsonObject(with: data!, options: []) as! [[Any]])
+            default:
+                self.showToast(msg: "오류 : \(code)")
+            }
+        })
+    }
+    
+    func bindData(_ dataArr: [[Any]]){
+        let width = dataArr[0].count * 65
+        let height = dataArr.count * 65
+        
+        let tempX = backScrollView.frame.width - CGFloat(width)
+        let tempY = backScrollView.frame.height - CGFloat(height)
+        
+        let setX = tempX > 0 ? tempX : 10
+        let setY = tempY > 0 ? tempY : 10
+        
+        let contentView = UIView.init(frame: CGRect.init(x: setX, y: setY, width: CGFloat(width), height: CGFloat(height)))
+        
+        var x = 0, y = 0
+        
+        for seatArr in dataArr{
+            for seat in seatArr{
+                let button = getButton(x: x, y: y, title: seat as! String)
+                if let titleInt = seat as? Int, titleInt > 0{
+                    button.setBackgroundImage(UIImage(named: "seatNo"), for: .normal)
+                }else{
+                    button.setBackgroundImage(UIImage(named: "seatYes"), for: .normal)
+                }
+                x += 65
+            }
+            y += 65
+        }
+        
+        backScrollView.contentSize = CGSize.init(width: width + 10, height: height + 10)
+        backScrollView.addSubview(contentView)
+    }
+    
+    func getButton(x: Int, y: Int, title: String) -> UIButton{
+        let button = UIButton.init(frame: CGRect.init(x: x, y: y, width: 55, height: 55))
+        button.setTitle(title, for: .normal)
+        button.addTarget(self, action: #selector(onClick(_:)), for: .touchUpInside)
+        
+        return button
+    }
+    
+    @objc func onClick(_ button: UIButton){
+        if let intTitle = button.title(for: .normal) as? Int{
+            beforeButton?.setBackgroundImage(UIImage.init(named: "seatNo"), for: .normal)
+            button.setBackgroundImage(UIImage.init(named: "seatSelect"), for: .normal)
+            selectedSeat = intTitle
+        }else{
+            showToast(msg: "자리가 있습니다.")
+        }
+    }
+    
+    
 }
