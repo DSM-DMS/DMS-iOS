69be164cbf1571b790ad634b9c89bc0fdc4ca43f
W.VErtex <galaxy000213@gmail.com>
2018-06-14 12:09:37 +0900

    Modify MealExtension

--- MealExtension/TodayViewController.swift
+++ MealExtension/TodayViewController.swift
@@ -17,15 +17,13 @@ class TodayViewController: UIViewController, NCWidgetProviding {
     @IBOutlet weak var dateLabel: UILabel!
     
     private let formatter = DateFormatter()
-    private let disposeBag = DisposeBag()
     
-    private var date: Date = Date()
+    private var date: Date!
     private let aDay = TimeInterval(86400)
     
-    private var dataArr = ["","",""]
-    private var timeStrArr = ["아침", "점심", "저녁"]
+    private var data = [String]()
     
-    private var curTime = 0
+    private var currentTime = 0
     
     override func viewDidLoad() {
         date = Date()
@@ -33,12 +31,12 @@ class TodayViewController: UIViewController, NCWidgetProviding {
     }
     
     @IBAction func before(_ sender: Any) {
-        curTime -= 1
+        currentTime -= 1
         setData()
     }
     
     @IBAction func after(_ sender: Any) {
-        curTime += 1
+        currentTime += 1
         setData()
     }
     
@@ -55,27 +53,27 @@ extension TodayViewController{
         guard let curIntTime = Int(formatter.string(from: date)) else{ return }
         switch curIntTime {
         case 0...8:
-            curTime = 1
+            currentTime = 0
         case 9...12:
-            curTime = 2
+            currentTime = 1
         case 13...17:
-            curTime = 3
+            currentTime = 2
         default:
-            date += aDay
-            curTime = 1
+            date! += aDay
+            currentTime = 0
         }
         getData()
     }
     
     private func setData(){
-        switch curTime {
-        case 0:
-            date -= aDay
-            curTime = 3
+        switch currentTime {
+        case -1:
+            date! -= aDay
+            currentTime = 2
             getData()
-        case 4:
-            date += aDay
-            curTime = 1
+        case 3:
+            date! += aDay
+            currentTime = 0
             getData()
         default:
             bind()
@@ -84,52 +82,26 @@ extension TodayViewController{
     
     private func getData(){
         formatter.dateFormat = "YYYY-MM-dd"
-        dateLabel.text = formatter.string(from: date)
-        var request = URLRequest(url: URL(string: "http://dsm2015.cafe24.com/meal/\(formatter.string(from: date))")!)
-        request.httpMethod = "GET"
-        URLSession.shared.dataTask(with: request){
-            data, res, err in
-            if err != nil{ return }
-            let code = (res as! HTTPURLResponse).statusCode
-            switch code{
-            case 200:
-                let decodeData = try! JSONDecoder().decode(MealModel.self, from: data!)
-                self.dataArr = decodeData.getData()
-            case 204:
-                self.dataArr = ["급식이 없습니다", "급식이 없습니다", "급식이 없습니다"]
-            default:
-                self.dataArr = ["오류 : \(code)", "오류 : \(code)", "오류 : \(code)"]
-            }
-            DispatchQueue.main.async { self.bind() }
-        }.resume()
+        let dateStr = formatter.string(from: date)
+        dateLabel.text = dateStr
+        _ = Connector.instance
+            .getRequest(InfoAPI.getMealInfo(date: dateStr), method: .get)
+            .decodeData(MealModel.self)
+            .subscribe(onNext: { [weak self] code, data in
+                guard let strongSelf = self else { return }
+                switch code{
+                case 200: strongSelf.data = data!.getDataForExtension()
+                case 204: strongSelf.data = ["급식이 없습니다", "급식이 없습니다", "급식이 없습니다"]
+                default: strongSelf.data = ["오류 : \(code)", "오류 : \(code)", "오류 : \(code)"]
+                }
+                strongSelf.bind()
+            })
     }
     
     func bind(){
-        timeLabel.text = timeStrArr[curTime - 1]
-        dataTextView.text = dataArr[curTime - 1]
-    }
-    
-}
-
-class MealModel: Codable{
-    
-    let breakfast: [String]
-    let lunch: [String]
-    let dinner: [String]
-    
-    func getData() -> [String]{
-        var dataArr = ["", "", ""]
-        dataArr[0] = getStr(breakfast)
-        dataArr[1] = getStr(lunch)
-        dataArr[2] = getStr(dinner)
-        return dataArr
-    }
-    
-    private func getStr(_ arr: [String]) -> String{
-        var dataStr = ""
-        for i in arr{ dataStr += i + ", " }
-        dataStr.removeLast(2)
-        return dataStr
+        print(currentTime)
+        timeLabel.text = ["아침", "점심", "저녁"][currentTime]
+        dataTextView.text = data[currentTime]
     }
     
 }
